917c0951c24c47541c99907882afb5b38ae1f4fe
sitalukk <sitalukk@jyu.fi>
2026-07-10 19:39:14 +0300

    Puuhattu tehtävää 7.4

--- src/main/java/fi/jyu/ohj2/simoL/todo/MainController.java
+++ src/main/java/fi/jyu/ohj2/simoL/todo/MainController.java
@@ -3,8 +3,10 @@ package fi.jyu.ohj2.simoL.todo;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,17 +19,41 @@ public class MainController implements Initializable {
     private TextField uusiTehtavaNimi;
 
     @FXML
-    private Label tekemattomat;
+    private VBox tekemattomat;
+
+    @FXML
+    private VBox tehdyt;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        uusiTehtavaNimi.requestFocus();
-        lisaaUusiTehtavaPainike.setOnAction(event -> {
-            String teksti = uusiTehtavaNimi.getText(); // Haetaan tekstikentän sisältö
-            System.out.println("Tekstikentän sisältö: " + teksti);
-            tekemattomat.setText(tekemattomat.getText() + teksti + "\n");
-            });
-        // Write initialization code here
+        lisaaUusiTehtavaPainike.setOnAction(event -> lisaaTehtava());
+        uusiTehtavaNimi.setOnAction(event -> lisaaTehtava());
     }
+
+    private void lisaaTehtava() {
+        String teksti = uusiTehtavaNimi.getText();
+        if (teksti == null || teksti.isBlank()) {
+            uusiTehtavaNimi.requestFocus();
+            return;
+        }
+        teksti = teksti.trim();
+        CheckBox tehtava = new CheckBox(teksti);
+        tehtava.setOnAction(event -> {
+            if (tehtava.isSelected()) { // Tehtävä valittu --> Siirretään tehtyjen joukkoon
+                tekemattomat.getChildren().remove(tehtava);
+                tehdyt.getChildren().add(tehtava);
+            } else { // Tehtävä ei-valittu--> Siirretään takaisin tekemättömien joukkoon
+                tehdyt.getChildren().remove(tehtava);
+                tekemattomat.getChildren().add(tehtava);
+            }
+        });
+
+        tekemattomat.getChildren().add(tehtava);
+        uusiTehtavaNimi.clear();
+        uusiTehtavaNimi.requestFocus();
+    }
+
+
+
 
 }
