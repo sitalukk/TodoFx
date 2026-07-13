a4756a9fb729f4a1eeb65ed69241710d1d9b7e4c
sitalukk <simo.t.lukkari@student.jyu.fi>
2026-07-13 11:38:14 +0300

    Tehtava 7.5

--- src/main/java/fi/jyu/ohj2/simoL/todo/MainController.java
+++ src/main/java/fi/jyu/ohj2/simoL/todo/MainController.java
@@ -7,8 +7,15 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import tools.jackson.core.JacksonException;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -26,8 +33,63 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        lataa();
         lisaaUusiTehtavaPainike.setOnAction(event -> lisaaTehtava());
         uusiTehtavaNimi.setOnAction(event -> lisaaTehtava());
+    }
+
+    private List<Tehtava> haeTehtavat(VBox sailio) {
+        return sailio.getChildren().stream()
+                .map(n -> (CheckBox) n)
+                .map(cb -> new Tehtava(cb.getText(), cb.isSelected()))
+                .toList();
+    }
+
+    private void tallenna() {
+        List<Tehtava> kaikkiTehtavat = new ArrayList<>();
+        kaikkiTehtavat.addAll(haeTehtavat(tekemattomat));
+        kaikkiTehtavat.addAll(haeTehtavat(tehdyt));
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(Path.of("tehtavat.json"), kaikkiTehtavat);
+    }
+
+    private void lataa() {
+        Path path = Path.of("tehtavat.json");
+        if (Files.notExists(path)) {
+            return;
+        }
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Tehtava> kaikkiTehtavat = mapper.readValue(path.toFile(), new TypeReference<>() {});
+            kaikkiTehtavat.forEach(tehtava -> {
+                CheckBox checkbox = luoCheckBox(tehtava.getTeksti(), tehtava.getTehty());
+                if (tehtava.getTehty()) {
+                    tehdyt.getChildren().add(checkbox);
+                } else {
+                    tekemattomat.getChildren().add(checkbox);
+                }
+            });
+        } catch (JacksonException je) {
+            System.out.println("JSONin lukeminen epäonnistui: " + je.getMessage());
+        }
+    }
+
+
+    private CheckBox luoCheckBox(String teksti,boolean valittu) {
+        CheckBox tehtava = new CheckBox(teksti);
+        tehtava.setSelected(valittu);
+        // metodin runko piilotettu...
+        tehtava.setOnAction(event -> {
+            if (tehtava.isSelected()) {
+                tekemattomat.getChildren().remove(tehtava);
+                tehdyt.getChildren().add(tehtava);
+            } else {
+                tehdyt.getChildren().remove(tehtava);
+                tekemattomat.getChildren().add(tehtava);
+            }
+            tallenna();
+        });
+        return tehtava;
     }
 
     private void lisaaTehtava() {
@@ -37,6 +99,7 @@ public class MainController implements Initializable {
             return;
         }
         teksti = teksti.trim();
+        tekemattomat.getChildren().add(luoCheckBox(teksti, false));
         CheckBox tehtava = new CheckBox(teksti);
         tehtava.setOnAction(event -> {
             if (tehtava.isSelected()) { // Tehtävä valittu --> Siirretään tehtyjen joukkoon
@@ -46,14 +109,15 @@ public class MainController implements Initializable {
                 tehdyt.getChildren().remove(tehtava);
                 tekemattomat.getChildren().add(tehtava);
             }
+            tallenna();
         });
+        
+            uusiTehtavaNimi.clear();
+            uusiTehtavaNimi.requestFocus();
 
-        tekemattomat.getChildren().add(tehtava);
-        uusiTehtavaNimi.clear();
-        uusiTehtavaNimi.requestFocus();
+            tallenna();
+        }
+
     }
 
 
-
-
-}
--- src/main/java/fi/jyu/ohj2/simoL/todo/Tehtava.java
+++ src/main/java/fi/jyu/ohj2/simoL/todo/Tehtava.java
@@ -1,4 +1,23 @@
 package fi.jyu.ohj2.simoL.todo;
 
 public class Tehtava {
+    @SuppressWarnings("FieldMayBeFinal")
+    private String teksti;
+    @SuppressWarnings("FieldMayBeFinal")
+    private boolean tehty;
+
+    @SuppressWarnings("unused")
+    public Tehtava() {
+        this.teksti = teksti;
+        this.tehty = tehty;/* Jätä tyhjäksi tai tee oletustoteutus */ }
+
+    public Tehtava(String teksti, boolean tehty) {
+        this.teksti = teksti;
+        this.tehty = tehty; /* Lisää toteutus */ }
+
+    public boolean getTehty() { /* Lisää toteutus */
+    return tehty;}
+
+    public String getTeksti() { /* Lisää toteutus */
+    return teksti;}
 }
